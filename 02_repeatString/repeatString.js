27e52685232f7f246c33dc98fa0c52345f7c1e8a
Marlon Ilao <[176732593+Marlonilao@users.noreply.github.com]>
2024-09-24 19:39:33 +0800

    finished repeatstring.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let strings = ""; 
+    let error = "ERROR"
+    if (num >= 0) {
+        for (let i=0 ; i<num ; i++) {
+        strings += string;
+        } return strings
+    } else if (num < 0) {
+        return error
+    }
 };
 
 // Do not edit below this line
