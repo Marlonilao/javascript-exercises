879df87be321c3881dc0fbd5f1d8889db2de627f
Marlon Ilao <[176732593+Marlonilao@users.noreply.github.com]>
2024-09-24 20:47:46 +0800

    finished reverseString.js exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+const reverseString = function(word) {
+    var splitWord = word.split("");
+    var reversedWord = splitWord.reverse();
+    var c = reversedWord.join("");
+    return c
+   }
 
 // Do not edit below this line
 module.exports = reverseString;
