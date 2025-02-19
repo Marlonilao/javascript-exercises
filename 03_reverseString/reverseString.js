297e0a9d0a595b76ef1675cedad4f44a21961ba3
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-02-19 21:17:53 +0800

    Complete javascript exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    array = word.split("")
+    result = []
+    for (i=0; i<array.length;i++) {
+        result.unshift(array[i])
+    }
+    return result.join("")
 };
 
 // Do not edit below this line
