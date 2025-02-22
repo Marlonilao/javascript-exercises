11b3955b9da10f84d4e918f8f6e7b49434359136
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-02-22 17:28:19 +0800

    Complete javascript exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number <= -1) return "OOPS";
+    let fibSeq = [0,1]
+    for (let i=0;i<number;i++) {
+        fibSeq.push(fibSeq[i]+fibSeq[i+1]);
+    };
+    return fibSeq[number];
 };
 
 // Do not edit below this line
